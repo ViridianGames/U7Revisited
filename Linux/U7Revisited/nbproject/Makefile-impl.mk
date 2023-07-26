ff4e7b371b5efe3c211c9c50c715d037868879b7
Steam Deck User <deck@steamdeck.lan>
2023-07-26 06:34:51 -0500

    Updating the Linux/Netbeans project of U7Revisited

--- Linux/U7Revisited/nbproject/Makefile-impl.mk
+++ Linux/U7Revisited/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Planitia
+PROJECTNAME=U7Revisited
 
 # Active Configuration
 DEFAULTCONF=Debug
--- Linux/U7Revisited/nbproject/Package-Release.bash
+++ Linux/U7Revisited/nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/planitia
-OUTPUT_BASENAME=planitia
-PACKAGE_TOP_DIR=planitia/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/u7revisited
+OUTPUT_BASENAME=u7revisited
+PACKAGE_TOP_DIR=u7revisited/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/planitia/bin"
+makeDirectory "${NBTMPDIR}/u7revisited/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/planitia.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/u7revisited.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/planitia.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/u7revisited.tar *
 checkReturnCode
 
 # Cleanup
