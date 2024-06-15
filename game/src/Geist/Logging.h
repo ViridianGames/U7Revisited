af536f270d61cc18f6749ae19d5b80a0c47186c6
erikyuzwa <erikyuzwa@gmail.com>
2024-06-15 06:45:49 -0600

    chore: updated doc

--- game/src/Geist/Logging.h
+++ game/src/Geist/Logging.h
@@ -4,7 +4,9 @@
 // Author:   Anthony Salter
 // Date:     2/03/05
 // Purpose:  Dirt-simple logging system.  Just call Log() with whatever
-//           text you wish to write to the log file.
+//           text you wish to write to the log file. An optional parameter
+//           is the level of logging. One of LOG_INFO, LOG_ERROR, LOG_WARNING,
+//           LOG_DEBUG. LOG_INFO is the default.
 //
 ///////////////////////////////////////////////////////////////////////////
 
