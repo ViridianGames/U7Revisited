48e44bbf6d6d1d4f264748911e7a3cef6aab7531
Roy Riggs <furroy@gmail.com>
2025-11-10 16:47:27 -0500

    fixing sealed box

--- Redist/Data/Scripts/object_sealedbox_0798.lua
+++ Redist/Data/Scripts/object_sealedbox_0798.lua
@@ -1,8 +1,7 @@
---- Best guess: Changes an item's type to 799 and sets its frame to 14 when event ID 1 is received, likely for a transformation or state change.
+--- SealedBox: Changes an item's type to 799 and sets its frame to 14 to unseal it
 function object_sealedbox_0798(eventid, objectref)
     if eventid == 1 then
-        --set_item_shape(799, objectref)
+        set_object_shape(objectref, 799)
         set_object_quality(objectref, 14)
     end
-    return
 end
