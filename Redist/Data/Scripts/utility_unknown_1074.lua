5b7ec1fd495d0dbdbe1bef44497c15dde2cf76eb
Roy Riggs <furroy@gmail.com>
2025-11-10 17:50:41 -0500

    fixing sextant script

--- Redist/Data/Scripts/utility_unknown_1074.lua
+++ Redist/Data/Scripts/utility_unknown_1074.lua
@@ -1,10 +1,8 @@
 --- Best guess: Normalizes an input value, ensuring it's positive by multiplying by -1 if negative.
-function utility_unknown_1074(eventid, objectref, arg1)
-    local var_0000
-
-    var_0000 = arg1
-    if var_0000 < 0 then
-        var_0000 = var_0000 * -1
+--- obsolete as you can call math.abs() in lua
+function utility_unknown_1074(arg1)
+    if arg1 < 0 then
+        arg1 = arg1 * -1
     end
-    return var_0000
+    return arg1
 end
